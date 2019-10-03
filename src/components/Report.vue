e15d7cabce88256d7810b88500224a15d9fd9297
qmzik <qmzik208@gmail.com>
2019-10-03 15:24:21 +0500

    ReportStatus componets created

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,17 +1,22 @@
 <template>
-    <ul class="grid list_default">
+    <ul class="report grid list_default">
         <li>{{ id }}</li>
         <li>{{ typeId }}</li>
         <li>{{ stringDate }}</li>
-        <li>{{ status }}</li>
+        <li><ReportStatus :status="status"></ReportStatus></li>
         <li><i class="icon-trash" @click="removeReport"></i></li>
     </ul>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { ReportModule } from '../store/modules/report';
 
-@Component
+@Component({
+    components: {
+        ReportStatus: () => import('@/components/ReportStatus.vue'),
+    },
+})
 export default class Report extends Vue {
     @Prop() private id!: string;
     @Prop() private typeId!: string;
@@ -21,9 +26,23 @@ export default class Report extends Vue {
     private get stringDate(): string {
         return new Date(this.date).toLocaleString('ru-RU');
     }
+
+    private removeReport(): void {
+        ReportModule.removeReport(this.id);
+    }
 }
 </script>
 
 <style scoped lang="scss">
 @import '../scss/commonStyles.scss';
+
+.report {
+    font-size: 14px;
+    padding: 15px 0;
+}
+
+.icon-trash {
+    font-size: 20px;
+    cursor: pointer;
+}
 </style>
